e9c84d8a178b3e65364087b0fa198e05c67c0879
Darika34 <darikaakmatsaeva@gmail.com>
2023-07-28 13:42:08 +0600

    email2

--- config/urls.py
+++ config/urls.py
@@ -40,8 +40,8 @@ urlpatterns = [
     path('reset-password', PasswordResetView.as_view(), name='password_reset'),
     path('reset-password/done', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset-password/confirm/<uidb64>[0-9A-Za-z]+)-<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # path('reset-password/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('accounts/reset/<uidb64>/<token>/', PasswordResetDoneView.as_view(), name='password_reset_confirm'),
+    path('reset-password/complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    # path('accounts/reset/<uidb64>/<token>/', PasswordResetDoneView.as_view(), name='password_reset_confirm'),
 
 
 ]
